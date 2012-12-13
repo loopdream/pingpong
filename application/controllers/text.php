a85a39e1f55218ddd3dea150943944b866b1bfbf
David George <david@setr.co.uk>
2012-12-13 15:46:50 +0000

    twilio hooks

--- application/controllers/text.php
+++ application/controllers/text.php
@@ -12,18 +12,24 @@ class Text extends CI_Controller
 		$this->load->model('mQueue', 'queue');
 		$this->load->model('mUser', 'user');
 
+		$_REQUEST['number'] = $_REQUEST['Caller'];
+		error_log($_REQUEST);
+
 		// get the user
-		$user = $this->user->get_by_number($_GET['number']);
+		$user = $this->user->get_by_number($_REQUEST['number']);
 		if (!$user)
 		{
-			$this->user->register_user($_GET);
-			$user = $this->user->get_by_number($_GET['number']);
+			$this->user->register_user($_REQUEST);
+			$user = $this->user->get_by_number($_REQUEST['number']);
 		}
 
 		$result = $this->queue->add_player($user);
 
-		// add them to the queue
-		echo '{ "message": "OK" }';
+	    header("content-type: text/xml");
+	    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		echo "<Response>";
+		echo "<Sms>you are in the queue babe x x x x</Sms>";
+		echo "</Response>";
 	}
 }
 
