6da1f1289064a3c0bd077b5ca9ba8455f978482e
Totoro Daiyo <totoro@totoro.(none)>
2012-12-13 06:19:47 +0000

    Added new javascrip - audio imlementation , new styles and mp3 files

--- application/views/counter.php
+++ application/views/counter.php
@@ -17,28 +17,88 @@
 
 
 	<body>
+	
 
-		<div id="player1" class="player">
-			<div class="score-card">
-				<span class="score">
-					<span class="number front current">0</span>
-					<span class="number back next">0</span>
-				</span>
+		
+		<div id="scores">
+
+			<div id="player1" class="player">
+		
+				<div class="score-card">
+					<span class="score">
+						<span class="number front current">0</span>
+						<span class="number back next">0</span>
+					</span>
+					 
+				</div>
+	 			<div class="player-name">
+						<span>@maracuja</span>
+				</div>
 			</div>
-			<span class="remove-btn btn" data-player="player1">- remove point</span>
+	
+			<div id="player2" class="player">
+			
+				<div class="score-card">
+					<span class="score">
+						<span class="number front current">0</span>
+						<span class="number back next">0</span>
+					</span>
+					 
+				</div>
+				
+									
+				<div class="player-name">
+						<span>@loopdream</span>
+				</div>
+				 
+			</div>	
+					
 		</div>
+		
 
-		<div id="player2" class="player">
-			<div class="score-card">
-				<span class="score">
-					<span class="number front current">0</span>
-					<span class="number back next">0</span>
-				</span>
-			</div>
-			<span class="remove-btn btn" data-player="player2">- remove point</span>
+	
+		<div id="player-queue">
+			
+			<ul>
+				<li>@maracuja</li>
+				<li>@loopdream</li>
+			</ul>
+			
 		</div>
-
-		<span class="reset-btn btn">Reset scores</span>
+		
+		<audio id="number-0" src="audio/0.mp3">
+		<audio id="number-1" src="audio/1.mp3">
+		<audio id="number-2" src="audio/2.mp3">
+		<audio id="number-3" src="audio/3.mp3">
+		<audio id="number-4" src="audio/4.mp3">
+		<audio id="number-5" src="audio/5.mp3">
+		<audio id="number-6" src="audio/6.mp3">	
+		<audio id="number-7" src="audio/7.mp3">
+		<audio id="number-8" src="audio/8.mp3">
+		<audio id="number-9" src="audio/9.mp3">
+		<audio id="number-10" src="audio/10.mp3">
+		<audio id="number-11" src="audio/11.mp3">
+		<audio id="number-12" src="audio/12.mp3">	
+		<audio id="number-13" src="audio/13.mp3">
+		<audio id="number-14" src="audio/14.mp3">
+		<audio id="number-15" src="audio/15.mp3">
+		<audio id="number-16" src="audio/16.mp3">
+		<audio id="number-17" src="audio/17.mp3">
+		<audio id="number-18" src="audio/18.mp3">
+		<audio id="number-19" src="audio/19.mp3">		
+		<audio id="number-20" src="audio/20.mp3">
+		<audio id="number-21" src="audio/21.mp3">
+		 
+		
+		<audio id="commentary-shit" src="audio/shit.mp3">
+		<audio id="commentary-awesome" src="audio/awesome.mp3">
+		<audio id="commentary-really_good" src="audio/really_good.mp3">
+		<audio id="commentary-shit_no_good" src="audio/shit_no_good.mp3">
+		<audio id="commentary-wft" src="audio/wft.mp3">
+		<audio id="commentary-come_on" src="audio/come_on.mp3">
+		<audio id="commentary-your_invincible" src="audio/your_invincible.mp3">
+		
+</audio>
 
 		<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 		<script type="text/javascript" src="js/jquery.flipCounter.1.2.pack.js"></script>
